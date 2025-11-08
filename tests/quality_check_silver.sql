5bc50f1f6da57cca929c103eaa78fe4f245395ae
Mhaqq11 <122860078+Mhaqq11@users.noreply.github.com>
2025-11-08 15:32:43 -0800

    Create quality_check_silver.sql

---
+++ tests/quality_check_silver.sql
@@ -0,0 +1,165 @@
+-- Check for Invalid Dates
+SELECT
+NULLIF(sls_ship_dt, 0) AS sls_ship_dt
+FROM bronze.crm_sales_details
+WHERE sls_ship_dt <= 0 
+OR LEN(sls_ship_dt) != 8
+OR sls_ship_dt > 20500101
+OR sls_ship_dt < 19000101
+
+
+-- Check for Invalid Date Orders
+SELECT * 
+FROM bronze.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt 
+OR sls_order_dt > sls_due_dt
+
+-- Check Data Consistency: Between Sales, Quantity, and Price
+-- >> Sales = Quantity * Price
+-- >> Values must not be NULL, zero, or negative
+
+SELECT DISTINCT
+sls_sales AS old_sls_slales,
+sls_quantity,
+sls_price AS old_sls_price,
+CASE 
+	WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
+	THEN sls_quantity * ABS(sls_price)
+	ELSE sls_sales
+END AS sls_sales,
+
+CASE 
+	WHEN sls_price IS NULL OR sls_price <= 0 
+	THEN sls_sales / NULLIF(sls_quantity,0)
+	ELSE sls_price
+END AS sls_price
+
+
+FROM bronze.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+ORDER BY sls_sales, sls_quantity, sls_price
+
+
+SELECT 
+cid,
+bdate,
+gen
+FROM bronze.erp_cust_az12
+
+-- Identify Out-of-Range Dates
+SELECT DISTINCT
+bdate
+FROM silver.erp_cust_az12
+WHERE bdate > GETDATE()
+
+-- Data Standardization & Consistency 
+SELECT DISTINCT gen
+FROM bronze.erp_cust_az12
+
+SELECT DISTINCT
+gen,
+CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+	 WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+	 ELSE 'n/a'
+END AS gen
+FROM bronze.erp_cust_az12
+
+
+SELECT DISTINCT gen
+FROM silver.erp_cust_az12
+
+
+-- Check for unwanted spaces
+-- Expectation: No Results
+
+SELECT cst_firstname
+FROM bronze.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname
+
+-- Check For Nulls or Duplicated in Primary Key
+-- Expectation: No Result
+SELECT
+cst_id,
+COUNT(*)
+FROM silver.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1
+
+-- Data Standardization & Consistency
+SELECT DISTINCT cst_gndr
+FROM silver.crm_cust_info
+
+SELECT DISTINCT cst_marital_status
+FROM silver.crm_cust_info
+
+-- Check for unwanted spaces
+-- Expectation: No Results
+
+SELECT cst_firstname
+FROM silver.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname)
+
+
+SELECT * FROM silver.crm_cust_info
+
+-- Check for NUlls or Negative Numbers
+-- Expectation: No Results
+SELECT prd_cost
+FROM bronze.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL
+
+--Data Standardization & Consistency
+SELECT DISTINCT prd_line FROM bronze.crm_prd_info
+
+-- Check For Nulls or Duplicates in Primary Key
+-- Expectation: No Results
+
+SELECT
+prd_id,
+COUNT(*)
+FROM bronze.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL
+
+-- Check for Invalid Date Orders
+SELECT * 
+FROM bronze.crm_prd_info
+WHERE prd_end_dt < prd_start_dt
+
+SELECT 
+prd_id,
+prd_key,
+prd_nm,
+prd_start_dt,
+prd_end_dt,
+LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) -1 AS prd_end_dt_test
+FROM bronze.crm_prd_info
+WHERE prd_key in ('AC-HE-HL-U509-R','AC-HE-HL-U509')
+
+SELECT * FROM silver.crm_prd_info
+
+
+-- Check For Nulls or Duplicated in Primary Key
+-- Expectation: No Result
+SELECT
+cst_id,
+COUNT(*)
+FROM bronze.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1
+
+-- Data Standardization & Consistency
+SELECT DISTINCT cst_gndr
+FROM bronze.crm_cust_info
+
+SELECT DISTINCT cst_marital_status
+FROM bronze.crm_cust_info
+
+-- Check for unwanted spaces
+-- Expectation: No Results
+
+SELECT cst_firstname
+FROM bronze.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname)
